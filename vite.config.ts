5a9e6fbecd9fb4cd5416e2e0a47a71e7937a19cc
paulius-ta <paulius.tartilas@gmail.com>
2024-02-13 22:19:38 +0200

    Update packages + config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@config': '/src/config',
+      '@pages': '/src/pages',
+      '@hooks': '/src/hooks',
+      '@types': '/src/types',
+      '@utils': '/src/utils',
+      '@styles': '/src/styles',
+      '@stores': '/src/stores',
+    },
+  },
+});
