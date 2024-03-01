3ebfe8a66c1187321b12f1de1c1a6476cadd125a
paulius-ta <paulius.tartilas@gmail.com>
2024-03-01 11:15:19 +0200

    Update Separator component

--- src/components/ui/Separator/Separator.tsx
+++ src/components/ui/Separator/Separator.tsx
@@ -3,22 +3,31 @@ import colors from '@styles/colors.ts';
 
 interface ComponentProps extends CustomComponent {
   space?: number;
+  vertical?: boolean;
 }
 
-const Root = styled.div<{$margin: number}>`
-  ${({$margin}) => css`
+const Root = styled.div<{$margin: number; $vertical?: boolean}>`
+  ${({$margin, $vertical}) => css`
     display: flex;
     height: 1px;
     justify-content: center;
     align-items: center;
     align-self: stretch;
-    background: ${colors.primarySeparator};
+    background: ${colors.background};
     margin: ${$margin}px 0;
+
+    ${$vertical &&
+    css`
+      height: unset;
+      width: 1px;
+      justify-self: stretch;
+      margin: 0 ${$margin}px;
+    `}
   `}
 `;
 
-const Separator = ({space = 0, className}: ComponentProps) => {
-  return <Root $margin={space} className={className} />;
+const Separator = ({space = 0, className, vertical}: ComponentProps) => {
+  return <Root className={className} $margin={space} $vertical={vertical} />;
 };
 
 export default Separator;
