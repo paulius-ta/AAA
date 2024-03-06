c5a18f025a5db3e8711a7eb55bf0f51465531637
paulius-ta <paulius.tartilas@gmail.com>
2024-03-06 12:06:56 +0200

    Add new icon to AssetsProvider

--- src/assets/AssetsProvider.ts
+++ src/assets/AssetsProvider.ts
@@ -3,9 +3,11 @@ import chevronDown from '@assets/icons/ic_chevron-down.svg?react';
 import description from '@assets/icons/ic_description.svg?react';
 import history from '@assets/icons/ic_history.svg?react';
 import star from '@assets/icons/ic_star.svg?react';
+import images from '@assets/icons/ic_images.svg?react';
 
 export const iconChevronRight = chevronRight;
 export const iconChevronDown = chevronDown;
 export const iconDescription = description;
 export const iconHistory = history;
 export const iconStar = star;
+export const iconImages = images;
