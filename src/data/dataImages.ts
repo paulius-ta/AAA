cc5b2c670300e89a28456f7e7ef2f7714f741118
paulius-ta <paulius.tartilas@gmail.com>
2024-03-06 12:05:22 +0200

    Add dataImages & new images

---
+++ src/data/dataImages.ts
@@ -0,0 +1,11 @@
+import {Images} from '@customTypes/model/apiTypes.ts';
+
+export const dataImages: Images = {
+  id: 90137109120,
+  images: [
+    {url: '/src/assets/images/artifact-1.png'},
+    {url: '/src/assets/images/artifact-2.png'},
+    {url: '/src/assets/images/artifact-3.png'},
+    {url: '/src/assets/images/artifact-4.png'},
+  ],
+};
