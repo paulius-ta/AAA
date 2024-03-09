15d5300a8e37b43ed3999143ac64d04e64b1278b
paulius-ta <paulius.tartilas@gmail.com>
2024-03-09 18:19:55 +0200

    Add icons + update AssetsProvider

--- src/assets/AssetsProvider.ts
+++ src/assets/AssetsProvider.ts
@@ -4,7 +4,9 @@ import description from '@assets/icons/ic_description.svg?react';
 import history from '@assets/icons/ic_history.svg?react';
 import star from '@assets/icons/ic_star.svg?react';
 import images from '@assets/icons/ic_images.svg?react';
-import orbit from '@assets/icons/ic_orbit.svg?react';
+import contact from '@assets/icons/ic_contact.svg?react';
+import payment from '@assets/icons/ic_payment.svg?react';
+import preview from '@assets/icons/ic_preview.svg?react';
 
 export const iconChevronRight = chevronRight;
 export const iconChevronDown = chevronDown;
@@ -12,4 +14,6 @@ export const iconDescription = description;
 export const iconHistory = history;
 export const iconStar = star;
 export const iconImages = images;
-export const iconOrbit = orbit;
+export const iconContact = contact;
+export const iconPayment = payment;
+export const iconPreview = preview;
