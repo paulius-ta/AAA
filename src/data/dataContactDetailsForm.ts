de9524a8f8c4180a6ba64546a5b27cf8a1d03726
paulius-ta <paulius.tartilas@gmail.com>
2024-03-16 14:12:01 +0200

    Add dataContactDetailsForm

---
+++ src/data/dataContactDetailsForm.ts
@@ -0,0 +1,69 @@
+export const phoneNumberOptions: SelectOption[] = [
+  {
+    value: 'AAX',
+    label: 'AAX',
+  },
+  {
+    value: 'ABY',
+    label: 'ABY',
+  },
+  {
+    value: 'ACZ',
+    label: 'ACZ',
+  },
+  {
+    value: 'ADV',
+    label: 'ADV',
+  },
+  {
+    value: 'AEQ',
+    label: 'AEQ',
+  },
+  {
+    value: 'AFO',
+    label: 'AFO',
+  },
+  {
+    value: 'AGS',
+    label: 'AGS',
+  },
+  {
+    value: 'AHI',
+    label: 'AHI',
+  },
+];
+
+export const locationCoordinateOptions: SelectOption[] = [
+  {
+    value: 'AA',
+    label: 'AA',
+  },
+  {
+    value: 'AB',
+    label: 'AB',
+  },
+  {
+    value: 'AC',
+    label: 'AC',
+  },
+  {
+    value: 'AD',
+    label: 'AD',
+  },
+  {
+    value: 'AE',
+    label: 'AE',
+  },
+  {
+    value: 'AF',
+    label: 'AF',
+  },
+  {
+    value: 'AG',
+    label: 'AG',
+  },
+  {
+    value: 'AH',
+    label: 'AH',
+  },
+];
