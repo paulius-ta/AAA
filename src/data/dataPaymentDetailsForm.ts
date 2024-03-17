7e165c2c2f8b39067a26ec4a01eca9efbc086a28
paulius-ta <paulius.tartilas@gmail.com>
2024-03-17 11:57:36 +0200

    Add payment method data, icon

---
+++ src/data/dataPaymentDetailsForm.ts
@@ -0,0 +1,24 @@
+import {PaymentMethods} from '@customTypes/model/apiTypes.ts';
+
+export const paymentMethods: PaymentMethods = [
+  {
+    url: '/src/assets/icons/ic_payment-method.svg',
+    value: 'method-1',
+  },
+  {
+    url: '/src/assets/icons/ic_payment-method.svg',
+    value: 'method-2',
+  },
+  {
+    url: '/src/assets/icons/ic_payment-method.svg',
+    value: 'method-3',
+  },
+  {
+    url: '/src/assets/icons/ic_payment-method.svg',
+    value: 'method-4',
+  },
+  {
+    url: '/src/assets/icons/ic_payment-method.svg',
+    value: 'method-5',
+  },
+];
