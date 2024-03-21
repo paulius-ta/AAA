4e5d1b8fad75defed1f3958850b43059c6f45b9d
paulius-ta <paulius.tartilas@gmail.com>
2024-03-21 13:25:55 +0200

    Update useContactDetailsTransition & usePaymentDetailsTransition

--- src/components/Wizard/transitions/useContactDetailsTransition.ts
+++ src/components/Wizard/transitions/useContactDetailsTransition.ts
@@ -1,13 +1,16 @@
 import {useStore} from '@stores/store.ts';
+import {useForm} from 'react-hook-form';
+import {ContactDetails} from '@customTypes/model/apiTypes.ts';
 
 const useContactDetailsTransition = () => {
   const {wizardStore} = useStore();
+  const {register, handleSubmit} = useForm<ContactDetails>();
 
-  const handleTransition = () => {
-    wizardStore.setStep(wizardStore.step + 1);
-  };
+  const handleTransition = handleSubmit(data => {
+    wizardStore.updateContactDetails(data);
+  });
 
-  return {handleTransition};
+  return {handleTransition, register};
 };
 
 export default useContactDetailsTransition;
--- src/components/Wizard/transitions/usePaymentDetailsTransition.ts
+++ src/components/Wizard/transitions/usePaymentDetailsTransition.ts
@@ -1,13 +1,24 @@
 import {useStore} from '@stores/store.ts';
+import {useForm} from 'react-hook-form';
+import {PaymentDetails} from '@customTypes/model/apiTypes.ts';
+import {reaction} from 'mobx';
 
 const usePaymentDetailsTransition = () => {
   const {wizardStore} = useStore();
+  const {register, handleSubmit, setValue} = useForm<PaymentDetails>();
 
-  const handleTransition = () => {
-    wizardStore.setStep(wizardStore.step + 1);
-  };
+  const handleTransition = handleSubmit(data => {
+    wizardStore.updatePaymentDetails(data);
+  });
 
-  return {handleTransition};
+  reaction(
+    () => wizardStore.paymentDetails.paymentMethod,
+    paymentMethod => {
+      setValue('paymentMethod', paymentMethod);
+    }
+  );
+
+  return {handleTransition, register, setValue};
 };
 
 export default usePaymentDetailsTransition;
