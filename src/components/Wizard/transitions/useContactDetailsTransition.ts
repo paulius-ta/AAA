b283c89790738582417c77b5fdf92d965995c8c6
paulius-ta <paulius.tartilas@gmail.com>
2024-03-28 15:58:15 +0200

    Add yupResolver & schema to useForm in useContactDetailsTransition

--- src/components/Wizard/transitions/useContactDetailsTransition.ts
+++ src/components/Wizard/transitions/useContactDetailsTransition.ts
@@ -1,18 +1,49 @@
 import {useStore} from '@stores/store.ts';
 import {useForm} from 'react-hook-form';
 import {ContactDetails} from '@customTypes/model/apiTypes.ts';
+import {yupResolver} from '@hookform/resolvers/yup';
+import {ObjectSchema, object, string, number} from 'yup';
+
+const contactDetailsSchema: ObjectSchema<ContactDetails> = object().shape({
+  name: string().required(),
+  mailId: string().email().required(),
+  phoneNumber: object().shape({
+    prefix: string().required(),
+    number: number().required().default(null),
+  }),
+  coordinates: object().shape({
+    lat: object().shape({
+      prefix1: string().required(),
+      prefix2: string().required(),
+      prefix3: string().required(),
+      coordinate: number().required().default(null),
+    }),
+    long: object().shape({
+      prefix1: string().required(),
+      prefix2: string().required(),
+      prefix3: string().required(),
+      coordinate: number().required().default(null),
+    }),
+  }),
+});
 
 const useContactDetailsTransition = () => {
   const {wizardStore} = useStore();
-  const {register, control, handleSubmit} = useForm<ContactDetails>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<ContactDetails>({
+    resolver: yupResolver(contactDetailsSchema),
+  });
 
   const handleTransition = handleSubmit(data => {
     wizardStore.updateContactDetails(data);
-    console.log(data);
     wizardStore.setStep(1);
   });
 
-  return {handleTransition, register, control};
+  return {handleTransition, register, control, errors};
 };
 
 export default useContactDetailsTransition;
