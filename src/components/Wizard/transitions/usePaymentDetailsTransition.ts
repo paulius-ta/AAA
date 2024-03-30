97696987948d5bf6ff207813d819feb2c4ff1f9b
paulius-ta <paulius.tartilas@gmail.com>
2024-03-30 15:37:01 +0200

    Add yupResolver & schema to useForm in usePaymentDetailsTransition

--- src/components/Wizard/transitions/usePaymentDetailsTransition.ts
+++ src/components/Wizard/transitions/usePaymentDetailsTransition.ts
@@ -3,12 +3,28 @@ import {useForm} from 'react-hook-form';
 import {PaymentDetails} from '@customTypes/model/apiTypes.ts';
 import {reaction} from 'mobx';
 import {useEffect} from 'react';
+import {number, object, ObjectSchema, string} from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
+
+const paymentDetailsSchema: ObjectSchema<PaymentDetails> = object().shape({
+  paymentMethod: string().required(),
+  cardNumber: number().required(),
+});
 
 const usePaymentDetailsTransition = () => {
   const {wizardStore} = useStore();
-  const {register, handleSubmit, setValue} = useForm<PaymentDetails>();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: {errors},
+  } = useForm<PaymentDetails>({
+    resolver: yupResolver(paymentDetailsSchema),
+    shouldFocusError: false,
+  });
 
   const handleTransition = handleSubmit(data => {
+    console.log(data);
     wizardStore.updatePaymentDetails(data);
     wizardStore.setStep(2);
   });
@@ -17,14 +33,14 @@ const usePaymentDetailsTransition = () => {
     const disposer = reaction(
       () => wizardStore.paymentDetails.paymentMethod,
       paymentMethod => {
-        setValue('paymentMethod', paymentMethod);
+        setValue('paymentMethod', paymentMethod, {shouldValidate: true});
       }
     );
 
     return () => disposer();
   }, []);
 
-  return {handleTransition, register, setValue};
+  return {handleTransition, register, setValue, errors};
 };
 
 export default usePaymentDetailsTransition;
