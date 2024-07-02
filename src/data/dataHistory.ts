504cfc95dacfc81871ea12c11c2ea47f4596b875
paulius-ta <paulius.tartilas@gmail.com>
2024-07-02 16:31:13 +0300

    Update dataHistory based on new schema

--- src/data/dataHistory.ts
+++ src/data/dataHistory.ts
@@ -1,11 +1,8 @@
 import {History} from '@customTypes/model/apiTypes.ts';
 
-export const dataHistory: History = {
-  id: 90137109120,
-  entries: [
-    {date: '2024-02-26', amount: '456'},
-    {date: '2024-02-27', amount: '457'},
-    {date: '2024-02-28', amount: '458'},
-    {date: '2024-02-29', amount: '459'},
-  ],
-};
+export const dataHistory: History = [
+  {timestamp: '2024-02-26', amount: '456'},
+  {timestamp: '2024-02-27', amount: '457'},
+  {timestamp: '2024-02-28', amount: '458'},
+  {timestamp: '2024-02-29', amount: '459'},
+];
