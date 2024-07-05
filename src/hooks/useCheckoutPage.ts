3867ed099c9f47ba8d65d4336ab12515b716eba4
paulius-ta <paulius.tartilas@gmail.com>
2024-07-05 17:19:43 +0300

    Add initial useCheckoutPage

---
+++ src/hooks/useCheckoutPage.ts
@@ -0,0 +1,33 @@
+import useFetchAuctionItem from '@hooks/api/useFetchAuctionItem.ts';
+import useFetchDescription from '@hooks/api/useFetchDescription.ts';
+import useFetchHistory from '@hooks/api/useFetchHistory.ts';
+
+const useCheckoutPage = () => {
+  const {
+    data: auctionItemData,
+    isError: isErrorAuctionItem,
+    isLoading: isLoadingAuctionItem,
+  } = useFetchAuctionItem();
+
+  const {
+    data: descriptionData,
+    isError: isErrorDescription,
+    isLoading: isLoadingDescription,
+  } = useFetchDescription();
+
+  const {
+    data: historyData,
+    isError: isErrorHistory,
+    isLoading: isLoadingHistory,
+  } = useFetchHistory();
+
+  return {
+    auctionItemData,
+    descriptionData,
+    historyData,
+    isError: isErrorAuctionItem || isErrorDescription || isErrorHistory,
+    isLoading: isLoadingAuctionItem || isLoadingDescription || isLoadingHistory,
+  };
+};
+
+export default useCheckoutPage;
