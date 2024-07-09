9b213ef43b25ab7ceda09a58470e49caa63ea121
paulius-ta <paulius.tartilas@gmail.com>
2024-07-09 20:59:08 +0300

    Remove scrolling-text keyframe

--- src/styles/animations.ts
+++ src/styles/animations.ts
@@ -1,14 +1,5 @@
 import {css} from 'styled-components';
 
-const animations = css`
-  @keyframes scrolling-text {
-    from {
-      transform: translateX(0);
-    }
-    to {
-      transform: translateX(calc(-100% - 100px));
-    }
-  }
-`;
+const animations = css``;
 
 export default animations;
