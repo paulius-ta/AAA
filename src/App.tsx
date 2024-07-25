dbeca27cc0360c7d015c42ce6cd48354d48abe19
paulius-ta <paulius.tartilas@gmail.com>
2024-07-25 17:00:13 +0300

    Update App with PageLayout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import {RouterProvider} from 'react-router-dom';
 import router from '@config/router.tsx';
 import GlobalStyle from '@styles/globalStyle.ts';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import PageLayout from '@layout/PageLayout.tsx';
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalStyle />
-      <RouterProvider router={router} />
+      <PageLayout>
+        <RouterProvider router={router} />
+      </PageLayout>
     </QueryClientProvider>
   );
 }
